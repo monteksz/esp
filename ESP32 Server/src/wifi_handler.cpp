3e451fb6749b1c1d11c1fa76d4ebd3d367bb78eb
Montekar <39762712+monteksz@users.noreply.github.com>
2025-04-10 19:59:19 +0700

    Mencoba konek setelah menerima data dan mempertahankan koneksi sampai 30 menit jika tidak ada data wifi bakal diskonek

--- ESP32 Server/src/wifi_handler.cpp
+++ ESP32 Server/src/wifi_handler.cpp
@@ -2,20 +2,20 @@
 #include "wifi_handler.h"
 
 // Ganti dengan SSID dan Password WiFi
-const char* ssid = "SSID WIFI";
-const char* password = "PASSWORD WIFI";
+const char* ssid = "SSID";
+const char* password = "Password";
 
 void connectToWiFi() {
-    WiFi.mode(WIFI_STA); // Pastikan ESP32 dalam mode Station
-    WiFi.disconnect();    // Pastikan tidak ada koneksi sebelumnya
+    if(WiFi.status() == WL_CONNECTED) {
+        Serial.println("WiFi sudah terhubung");
+        return;
+    }
 
-    // Tambahkan DNS Google untuk memperbaiki masalah DNS
-    WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE, IPAddress(8, 8, 8, 8), IPAddress(8, 8, 4, 4));
-
+    WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, password);
     Serial.print("Menghubungkan ke WiFi...");
 
-    int maxRetries = 30; // Timeout setelah 30 detik
+    int maxRetries = 30;
     while (WiFi.status() != WL_CONNECTED && maxRetries > 0) {
         Serial.print(".");
         delay(1000);
@@ -26,15 +26,22 @@ void connectToWiFi() {
         Serial.println("\nTerhubung ke WiFi!");
         Serial.print("IP Address: ");
         Serial.println(WiFi.localIP());
-
         Serial.print("MAC Address ESP32 Server: ");
         Serial.println(WiFi.macAddress());
-
         Serial.print("WiFi Channel: ");
         Serial.println(WiFi.channel());
     } else {
         Serial.println("\nGagal terhubung ke WiFi! Restarting ESP32...");
         delay(5000);
-        ESP.restart(); // Restart ESP32 jika gagal konek
+        ESP.restart();
     }
+}
+
+bool isWiFiConnected() {
+    return WiFi.status() == WL_CONNECTED;
+}
+
+void disconnectWiFi() {
+    WiFi.disconnect(true);
+    Serial.println("WiFi diputuskan");
 }
